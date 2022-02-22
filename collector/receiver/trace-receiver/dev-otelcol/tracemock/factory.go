41f2eab0f5a6f98c58ce78483f17cb7c8dcfb7c5
Renato Quedas <renato.quedas@appdynamics.com>
2022-02-22 10:56:22 -0600

    starting the factory implementation details

---
+++ collector/receiver/trace-receiver/dev-otelcol/tracemock/factory.go
@@ -0,0 +1,10 @@
+package tracemock
+
+import (
+	"go.opentelemetry.io/collector/component"
+)
+
+// NewFactory creates a factory for tracemock receiver.
+func NewFactory() component.ReceiverFactory {
+
+}
