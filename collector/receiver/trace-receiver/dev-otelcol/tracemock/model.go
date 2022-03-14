9f69ecca66ee36ba1c65e9812a11608c8dff6183
Renato Quedas <renato.quedas@appdynamics.com>
2022-03-14 16:53:05 -0700

    Telemetry Source types created and ready to be mapped into Resource

--- collector/receiver/trace-receiver/dev-otelcol/tracemock/model.go
+++ collector/receiver/trace-receiver/dev-otelcol/tracemock/model.go
@@ -1,6 +1,10 @@
 package tracemock
 
-import "go.opentelemetry.io/collector/model/pdata"
+import (
+	"go.opentelemetry.io/collector/model/pdata"
+	"math/rand"
+	"time"
+)
 
 type Atm struct{
     ID           int64
@@ -11,7 +15,7 @@ type Atm struct{
 	ISPNetwork   string
 }
 
-type Backendsystem struct{
+type BackendSystem struct{
 	Version       string
 	ProcessName   string
 	OSType        string
@@ -21,8 +25,63 @@ type Backendsystem struct{
 	Endpoint      string
 }
 
+func generateAtm() Atm{
+	newAtm := Atm{
+		ID: 111,
+		Name: "ATM-111-IL",
+		SerialNumber: "atmxph-2022-111",
+		Version: "v1.0",
+	}
+	i := getRandomNumber(1, 2)
+
+	switch i {
+	case 1:
+		newAtm.ISPNetwork = "comcast-sanfrancisco"
+		newAtm.StateID = "CA"
+	case 2:
+		newAtm.ISPNetwork = "comcast-chicago"
+		newAtm.StateID = "IL" 
+	}
+
+	return newAtm
+}
+
+func generateBackendSystem() BackendSystem{
+    i := getRandomNumber(1, 3)
+
+	newBackend := BackendSystem{
+    	ProcessName: "accounts",
+		Version: "v2.5",
+	}
+
+	switch i {
+		case 1:
+		 	newBackend.Endpoint = "accounts/api/v2.5/balance"
+		case 2:
+		  	newBackend.Endpoint = "accounts/api/v2.5/deposit"
+		case 3:
+			newBackend.Endpoint = "accounts/api/v2.5/withdrawn"
+
+	}
+
+	return newBackend
+}
+
+func getRandomNumber(min int, max int) int {
+	rand.Seed(time.Now().UnixNano())
+	i := (rand.Intn(max - min + 1) + min)
+    return i	
+} 
+
 func generateTraces() pdata.Traces{
 	traces := pdata.NewTraces()
-	
+	newAtm := generateAtm()
+	newBackendSystem := generateBackendSystem()
+
+	resourceSpan := traces.ResourceSpans().AppendEmpty()
+	atmResource := resourceSpan.Resource()
+
 	return traces
 }
+
+
